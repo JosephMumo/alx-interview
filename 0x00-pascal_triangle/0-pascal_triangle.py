c903cb6666517059caf301b0d2278a4395474ab8
JosephMumo <josephmumo25@gmail.com>
2023-03-24 06:33:05 -0700

    new

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
-	if(n <= 0) {
-		return [];
-	} else {
-		for i in range(n+1):
-      			for j in range(n-i):
-         			print(' ', end='')
+"""Pascal Triangle Interview Challenge"""
 
-      			C = 1
-      			for j in range(1, i+1):
-         			print(C, ' ', sep='', end='')
-         			C = C * (i - j) // j
-      			print()
-	}
+
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
