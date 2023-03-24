3d96a7aec0f8db1603f706e82e991246b9222da8
JosephMumo <josephmumo25@gmail.com>
2023-03-24 06:26:27 -0700

    init

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+	if(n <= 0) {
+		return [];
+	} else {
+		for i in range(n+1):
+      			for j in range(n-i):
+         			print(' ', end='')
+
+      			C = 1
+      			for j in range(1, i+1):
+         			print(C, ' ', sep='', end='')
+         			C = C * (i - j) // j
+      			print()
+	}
